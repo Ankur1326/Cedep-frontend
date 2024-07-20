a2a2c1a506dbfa89135906297e1781f091fc8e9e
Ankur1326 <swamiankur46@gmail.com>
2024-07-20 14:18:09 +0530

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import PrivateRoute from './components/PrivateRoute'
 import HomePage from './pages/HomePage'
+import DashboardPage from './pages/DashboardPage'
+import StudentPage from './pages/StudentPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PrivateRoute><HomePage /></PrivateRoute>,
+    children: [
+      { path: 'dashboard', element: <DashboardPage /> },
+      { path: 'student', element: <StudentPage /> },
+    ],
+  },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/register', element: <RegisterPage /> },
+]);
+
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/' element={
-          <PrivateRoute>
-            <HomePage />
-          </PrivateRoute>
-        } />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   )
 }
 
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
     
-    // const token = localStorage.getItem('adminToken');
-    return false ? children : <Navigate to="/login" />
+    const token = localStorage.getItem('accessToken');
+    return token ? children : <Navigate to="/login" />
 };
 
 export default PrivateRoute;
