7f568c12469f696799faa4b366d3b6e738e796fa
Ankur1326 <swamiankur46@gmail.com>
2024-07-21 08:26:18 +0530

    add rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
   },
-  base: '/' // Ensure the base path is correct
+  base: '/', // Ensure the base path is correct
 });
